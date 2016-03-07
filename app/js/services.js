e43c9143eae53df4b4b7747b0347c89c8513158d
Murph <linmurph@gmail.com>
2016-03-07 16:46:36 -0600

    A few changes to the subreddits.

--- app/js/services.js
+++ app/js/services.js
@@ -9,7 +9,7 @@ reddimgurServices.factory('Imgur', ['$resource',
     return $resource('https://api.imgur.com/3/gallery:subreddit', {}, {
       query: {
       	method:'GET',
-      	params:{'subreddit': '/r/pics'},
+      	params:{'subreddit': '/r/meme'},
       	isArray:false,
       	headers: {'Authorization': 'Client-ID 8c83be9ff273b18'}
       }

6a472bc2a20d883a3e7c74312d8cc0fa0eb60f34
Linda Murphy <linmurph@gmail.com>
2016-03-10 08:35:16 -0600

    Refactored controllers and web services to use promise chaining, and added card interfaces for images.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,52 +4,106 @@
 
 var reddimgurApp = angular.module('reddimgurControllers', []);
 
-reddimgurApp.controller('ImgurCtrl', ['$scope', 'Imgur', 'Reddit',
-  function ($scope, Imgur, Reddit) {
-    var COLORS = ['#ffebee', '#ffcdd2', '#ef9a9a', '#e57373', '#ef5350', '#f44336', '#e53935', '#d32f2f', '#c62828', '#b71c1c', '#ff8a80', '#ff5252', '#ff1744', '#d50000', '#f8bbd0', '#f48fb1', '#f06292', '#ec407a', '#e91e63', '#d81b60', '#c2185b', '#ad1457', '#880e4f', '#ff80ab', '#ff4081', '#f50057', '#c51162', '#e1bee7', '#ce93d8', '#ba68c8', '#ab47bc', '#9c27b0', '#8e24aa', '#7b1fa2', '#4a148c', '#ea80fc', '#e040fb', '#d500f9', '#aa00ff', '#ede7f6', '#d1c4e9', '#b39ddb', '#9575cd', '#7e57c2', '#673ab7', '#5e35b1', '#4527a0', '#311b92', '#b388ff', '#7c4dff', '#651fff', '#6200ea', '#c5cae9', '#9fa8da', '#7986cb', '#5c6bc0', '#3f51b5', '#3949ab', '#303f9f', '#283593', '#1a237e', '#8c9eff', '#536dfe', '#3d5afe', '#304ffe', '#e3f2fd', '#bbdefb', '#90caf9', '#64b5f6', '#42a5f5', '#2196f3', '#1e88e5', '#1976d2', '#1565c0', '#0d47a1', '#82b1ff', '#448aff', '#2979ff', '#2962ff', '#b3e5fc', '#81d4fa', '#4fc3f7', '#29b6f6', '#03a9f4', '#039be5', '#0288d1', '#0277bd', '#01579b', '#80d8ff', '#40c4ff', '#00b0ff', '#0091ea', '#e0f7fa', '#b2ebf2', '#80deea', '#4dd0e1', '#26c6da', '#00bcd4', '#00acc1', '#0097a7', '#00838f', '#006064', '#84ffff', '#18ffff', '#00e5ff', '#00b8d4', '#e0f2f1', '#b2dfdb', '#80cbc4', '#4db6ac', '#26a69a', '#009688', '#00897b', '#00796b', '#00695c', '#a7ffeb', '#64ffda', '#1de9b6', '#00bfa5', '#e8f5e9', '#c8e6c9', '#a5d6a7', '#81c784', '#66bb6a', '#4caf50', '#43a047', '#388e3c', '#2e7d32', '#1b5e20', '#b9f6ca', '#69f0ae', '#00e676', '#00c853', '#f1f8e9', '#dcedc8', '#c5e1a5', '#aed581', '#9ccc65', '#8bc34a', '#7cb342', '#689f38', '#558b2f', '#33691e', '#ccff90', '#b2ff59', '#76ff03', '#64dd17', '#f9fbe7', '#f0f4c3', '#e6ee9c', '#dce775', '#d4e157', '#cddc39', '#c0ca33', '#afb42b', '#9e9d24', '#827717', '#f4ff81', '#eeff41', '#c6ff00', '#aeea00', '#fffde7', '#fff9c4', '#fff59d', '#fff176', '#ffee58', '#ffeb3b', '#fdd835', '#fbc02d', '#f9a825', '#f57f17', '#ffff8d', '#ffff00', '#ffea00', '#ffd600', '#fff8e1', '#ffecb3', '#ffe082', '#ffd54f', '#ffca28', '#ffc107', '#ffb300', '#ffa000', '#ff8f00', '#ff6f00', '#ffe57f', '#ffd740', '#ffc400', '#ffab00', '#fff3e0', '#ffe0b2', '#ffcc80', '#ffb74d', '#ffa726', '#ff9800', '#fb8c00', '#f57c00', '#ef6c00', '#e65100', '#ffd180', '#ffab40', '#ff9100', '#ff6d00', '#fbe9e7', '#ffccbc', '#ffab91', '#ff8a65', '#ff7043', '#ff5722', '#f4511e', '#e64a19', '#d84315', '#bf360c', '#ff9e80', '#ff6e40', '#ff3d00', '#dd2c00', '#d7ccc8', '#bcaaa4', '#795548', '#d7ccc8', '#bcaaa4', '#8d6e63', '#eceff1', '#cfd8dc', '#b0bec5', '#90a4ae', '#78909c', '#607d8b', '#546e7a', '#cfd8dc', '#b0bec5', '#78909c'];
+reddimgurApp.controller('ImgurCtrl', ['$scope', '$http', '$q',
+  function ($scope, $http, $q, ImgurService, RedditService) {
+    $scope.images = [];
+    $scope.currentSub = 'pics';
+    $http.get('./subreddits.json')
+      .then(function(data) {
+        $scope.subreddit = data.data;
+        $scope.currentSub = data.data[0];
+      });
 
-    $scope.reddit = Reddit.query();
-    $scope.reddit.$promise.then( function(result) {
-      $scope.currentSub = result.data.children[0];
-    });
-    $scope.pics = Imgur.query();
-    $scope.pics.$promise.then( function(result) {
-      formatPics(result);
-    });
+    var subreddit = 'r/pics';
+    getSubreddit(subreddit);
+
+    $scope.enhance = false;
 
     $scope.selectSub = function(sub) {
-      console.log(sub.data.url);
       $scope.currentSub = sub;
+      getSubreddit(sub.data.url);
+    };
 
-      $scope.pics = Imgur.query({'subreddit': sub.data.url});
-      $scope.pics.$promise.then( function(result) {
-        formatPics(result);
+    $scope.viewInImgur = function(pic, $event) {
+      window.open('http://imgur.com/gallery/' + pic.data.data.id);
+
+      if ($event.stopPropagation) $event.stopPropagation();
+      if ($event.preventDefault) $event.preventDefault();
+      $event.cancelBubble = true;
+      $event.returnValue = false;
+    };
+
+    $scope.viewInReddit = function(permalink, $event) {
+      window.open('http://www.reddit.com' + permalink);
+
+      if ($event.stopPropagation) $event.stopPropagation();
+      if ($event.preventDefault) $event.preventDefault();
+      $event.cancelBubble = true;
+      $event.returnValue = false;
+    };
+
+    function getSubreddit(subreddit) {
+      $http.jsonp('http://www.reddit.com/' + subreddit +'.json?jsonp=JSON_CALLBACK')
+      .then(function(data) {
+        for (var i in data.data.data.children) {
+          if ((!(data.data.data.children[i].data.domain.includes('imgur')) ||
+            data.data.data.children[i].data.url.includes('gallery')))  {
+            data.data.data.children.splice(i, 1);
+          }
+        }
+
+        var ids = getIdArray(data.data.data.children);
+        $http.defaults.headers.common.Authorization = 'Client-ID 8c83be9ff273b18';
+        getAllLinks(ids).then(function(datas) {
+          $scope.imgur = formatPics(datas);
+        }).catch(function(error) {
+          console.log('error', error);
+        });
+      }).catch(function(error) {
+        console.log('error', error);
       });
-    };
+    }
 
-    $scope.enhance = function(pic) {
-      if (pic.span > 4) {
-        return 4;
-      } else {
-        return pic.span;
+    function getAllLinks(ids) {
+      var promises = ids.map(function(id) {
+        return $http.get('https://api.imgur.com/3/image/' + id);
+      });
+      return $q.all(promises);
+    }
+
+    function getIdArray(obj) {
+      var idArray = [];
+
+      for (var i in obj) {
+          var link = obj[i].data.url;
+          var slash = link.indexOf("/", 12);
+          var dot = link.indexOf(".", slash);
+          var id = (dot > 0 ? link.slice(slash + 1, dot) : link.slice(slash + 1));
+
+          if (id.length > 8) {
+            obj.splice(i, 1);
+          } else {
+            idArray.push(id);
+          }
       }
-    };
+      $scope.reddit = obj;
+      return idArray;
+    }
 
     function formatPics(obj) {
-      for (var pic in obj.data) {
-        var link = obj.data[pic].link;
+      for (var i in obj) {
+        var link = obj[i].data.data.link;
         var dot = link.indexOf(".", 20);
         var size = randomSize();
 
-        obj.data[pic].thumb = [link.slice(0, dot), size, link.slice(dot)].join('');
-        obj.data[pic].color = randomColor();
-        obj.data[pic].span = getSpan(size);
+        obj[i].data.data.thumb = [link.slice(0, dot), size, link.slice(dot)].join('');
+        obj[i].data.data.span = getSpan(size);
+        obj[i].data.data.permalink = $scope.reddit[i].data.permalink;
+        obj[i].data.data.title = $scope.reddit[i].data.title;
       }
+      return obj;
     }
 
-    function randomColor() {
-      return COLORS[Math.floor(Math.random() * COLORS.length)];
-    }
 
     function randomSize() {
       var r = Math.random();
--- app/js/services.js
+++ app/js/services.js
@@ -2,26 +2,38 @@
 
 /* Services */
 
-var reddimgurServices = angular.module('reddimgurServices', ['ngResource']);
+var reddimgurServices = angular.module('reddimgurServices', []);
 
-reddimgurServices.factory('Imgur', ['$resource',
-  function($resource){
-    return $resource('https://api.imgur.com/3/gallery:subreddit', {}, {
-      query: {
-      	method:'GET',
-      	params:{'subreddit': '/r/pics'},
-      	isArray:false,
-      	headers: {'Authorization': 'Client-ID 8c83be9ff273b18'}
+reddimgurServices.factory('RedditService', ['$http', '$q', function($http, $q, config) {
+    return {
+      getRedditPosts: function(config) {
+        return $http.get('https://www.reddit.com/r/:sub.json', {'sub': config.sub})
+          .then(function(response) {
+            if (typeof response.data === 'object') {
+              return response.data;
+            } else {
+              return $q.reject(response.data);
+            }
+          }, function(response) {
+            return $q.reject(response.data);
+          });
       }
-    });
-  }]);
+  };
+}]);
 
-reddimgurServices.factory('Reddit', ['$resource',
-  function($resource){
-    return $resource('./subreddits.json', {}, {
-      query: {
-        method:'GET',
-        isArray:false
+reddimgurServices.factory('ImgurService',['$http', '$q', function($http, $q, config) {
+    return {
+      getImage: function(config) {
+        return $http.get('https://api.imgur.com/3/image/:id', {'id': config})
+          .then(function(response) {
+            if (typeof response.data === 'object') {
+              return response.data;
+            } else {
+              return $q.reject(response.data);
+            }
+          }, function(response) {
+            return $q.reject(response.data);
+          });
       }
-    });
-  }]);
+  };
+}]);
